7dd6dda7dc6b14b1174fa3434f6c2eba5b5f44c7
Danielle Zevitz <dezevitz@gmail.com>
2020-06-17 16:10:18 -0400

    Initiallize share box

--- viewer2/js/share.js
+++ viewer2/js/share.js
@@ -1,5 +1,24 @@
-function sharePDF(){
-    alert("share pressed");
-}
+/* This contains the functionality for all of the share box 
+Features:
+-Opens on button click
 
+Todo:
+-make better visually
+-close
+
+*/
+
+//Function that opens the share box when share button is clicked
+function sharePDF(){
+    var box = document.getElementById("share-box");
+    box.style.visibility = "visible";
+}
 document.getElementById('share').addEventListener('click', sharePDF);
+
+
+//Function that closes the share box when x button is clicked
+function closeShare(){
+    var box = document.getElementById("share-box");
+    box.style.visibility = "hidden";
+}
+document.getElementById('share-close').addEventListener('click', closeShare);
