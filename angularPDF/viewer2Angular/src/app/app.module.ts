e7d3df9da42f2693d8fb2df1fd7b5dca6296852c
Danielle Zevitz <dezevitz@gmail.com>
2020-07-02 18:01:32 -0400

    end of 7/2

--- angularPDF/viewer2Angular/src/app/app.module.ts
+++ angularPDF/viewer2Angular/src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import{PdfViewerModule} from 'ng2-pdf-viewer';
+import { InlineSVGModule } from 'ng-inline-svg';
 import { from } from 'rxjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PdfViewerModule,
-    HttpClientModule
+    PdfViewerModule, //ng2-pdf-viewer module to view pdf
+    HttpClientModule, // this allows use the http of the pdf
+    InlineSVGModule.forRoot() 
   ],
   providers: [],
   bootstrap: [AppComponent]
